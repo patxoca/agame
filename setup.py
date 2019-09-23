d53550ca1ff8df86c88d21d454a81787f2b37bb8
Alexis Roda <alexis.roda.villalonga@gmail.com>
2019-09-23 09:10:25 +0200

    Add dependencies.

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
+        "numpy",
+        "pygame",
     ],
     entry_points="""
     # -*- Entry points: -*-
